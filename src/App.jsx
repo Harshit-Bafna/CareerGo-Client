b4a0f93c9a99544182e7ea5fff70576e9e71f6dd
Priyanshi Jain <priyanshij2534@gmail.com>
2025-01-14 01:39:51 +0530

    refactor: Login Page upadte

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-    return <h1 className="text-3xl font-bold underline">Home page</h1>
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Home page</h1> 
+            <h2 className="text-xl font-bold underline">Home page</h2> 
+        </>
+    );
 }
 
 export default App
