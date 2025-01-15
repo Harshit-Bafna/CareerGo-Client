b91d1d1bb48eda70c710a688a20cb50358c37a5d
Priyanshi Jain <priyanshij2534@gmail.com>
2025-01-15 11:48:51 +0530

    refactor: Login page and asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Login from './pages/login'
 
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Home page</h1> 
-            <h2 className="text-xl font-bold underline">Home page</h2> 
+        <Login/>
+            {/* <h1 className="text-3xl font-bold underline">Home page</h1> 
+            <h2 className="text-xl font-bold underline">Home page</h2>  */}
         </>
     );
 }
