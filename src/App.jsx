a5d36ee98ad5ef013e449bc93c37a38a7db5e576
Priyanshi Jain <priyanshij2534@gmail.com>
2025-01-15 11:51:26 +0530

    refactor: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Login from './pages/login'
 
 function App() {
     return (
         <>
-        <Login/>
-            {/* <h1 className="text-3xl font-bold underline">Home page</h1> 
-            <h2 className="text-xl font-bold underline">Home page</h2>  */}
+            <h1 className="text-3xl font-bold underline">Home page</h1> 
+            <h2 className="text-xl font-bold underline">Home page</h2> 
         </>
     );
 }
